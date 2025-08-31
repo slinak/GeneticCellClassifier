b84bb88ef98ee0a6c9c1feb08c95c2a3f562ef24
Scott Linak <scott@linak.org>
2025-08-31 12:19:42 -0700

    added hideous foreach foreach loop to associate things into the original sample data

--- GeneticCellClassifier/src/App.vue
+++ GeneticCellClassifier/src/App.vue
@@ -1,29 +1,42 @@
 <template>
-  <h1>Upload Here</h1>
-
-  <file-handler>
-
-  </file-handler>
-  <span>{{ cellClassifications }}</span>
+  <h1>Click to get Positive Associated Cell Classifications</h1>
+  <input type="button" value="Click" v-on:click="getPositiveClassifications" />
+  
+  <table>
+    <tr v-for="(c) in content">
+      <td>{{ c }}</td>
+    </tr>
+  </table>
 
 </template>
 
 <script>
+import { ref, onMounted } from 'vue';
 
-import { onMounted } from 'vue';
-
+const displayElement = ref(null);
 
 export default{
   setup(){
-    var simplifiedTestData, positiveClassificationsToCellTypes, cellClassifications;
+    let content = ref(0);
+    
+    var simplifiedTestData, positiveClassificationsToCellTypes, cellClassifications, associatedTestData;
     onMounted(async () => {
       simplifiedTestData = await ParseAndSimplifyTestData();
       positiveClassificationsToCellTypes = await ParseAndBuildClassificationDictionary();
       cellClassifications = await AssociatePositiveClassificationsToTestData();
 
-      console.log("Positive Associated Cell Classifications:");
-      console.log(cellClassifications);
+      associatedTestData = await GetTestData();
+
+      cellClassifications.forEach(c => {
+        associatedTestData.forEach(td => {
+          td["Positive Classification"] = c;
+        });
+      });
     });
+
+    function getPositiveClassifications() {
+      content.value = associatedTestData;
+    }
 
     async function AssociatePositiveClassificationsToTestData() {
       var associatedData = new Map();
@@ -32,34 +45,42 @@ export default{
 
       return associatedData;
     }
-    function GetClassificationFromData(sampleClassifications) {
-        var sampleClassification = new Map();
-        
-        sampleClassifications.forEach(sc => { sampleClassification.set(sc, positiveClassificationsToCellTypes.get(sc)); });
 
-        return sampleClassification;
+    function GetClassificationFromData(sampleClassifications) {
+        var sampleClassification = [];
+        
+        sampleClassifications.forEach(sc => {
+          sampleClassification = sampleClassification.concat(positiveClassificationsToCellTypes.get(sc));
+        });
+
+        return sampleClassification.join(", ");
     }
     
     async function ParseAndBuildClassificationDictionary() {
         var classifications = JSON.parse(await GetJsonFileContents('./qupath_cell_classification_with_trained_object_classifiers.json'));
         var newPositiveClassifications = new Map();
 
-        classifications.forEach(classification => Object.keys(classifications).forEach(k => {
-            if (classifications[k] == 1)
-                newPositiveClassifications.has(k) ? newPositiveClassifications.get(k).push(classifications['Cell Type']) : newPositiveClassifications.set(k, [classifications['Cell Type']]);
+        classifications.forEach(classification => Object.keys(classification).forEach(k => {
+            if (classification[k] == 1)
+              newPositiveClassifications.has(k) ? newPositiveClassifications.get(k).push(classification['Cell Type']) : newPositiveClassifications.set(k, [classification['Cell Type']]);
         }));
 
         return newPositiveClassifications;
     }
 
+    async function GetTestData() {
+      return JSON.parse(await GetJsonFileContents('./test data set.json'));
+    }
+
     async function ParseAndSimplifyTestData() {
         let testData = JSON.parse(await GetJsonFileContents('./test data set.json'));
         var newSimplifiedData = [];
+        var separator = new RegExp(' :|: ');
         
         testData.forEach(td => {
             newSimplifiedData.push({
                 "Object ID": td["Object ID"],
-                "Classification": td["Classification"].split(': '),
+                "Classification": td["Classification"].split(separator),
             });
         });
         return newSimplifiedData;
@@ -75,7 +96,7 @@ export default{
         })
         return JSON.stringify(fileContents);
     }
-    return { positiveClassificationsToCellTypes, simplifiedTestData, cellClassifications };
+    return { getPositiveClassifications, content };
   }
 }
 </script>
